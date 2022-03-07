f05665d06ab538acc657cec35650e1f10f3043c5
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-03-07 10:04:37 +0400

    added context processor navbar

---
+++ profiles/context_processors.py
@@ -0,0 +1,10 @@
+from .models import *
+
+#!profilePicViewContext function
+def profilePicViewContext(request):
+    if request.user.is_authenticated:
+        profile_obj = Profile.objects.get(user=request.user)
+        pic_user_navbar = profile_obj.avatar
+        return {'pic_user_navbar':pic_user_navbar}
+    #else well => return{} = else yeni 
+    return{}
