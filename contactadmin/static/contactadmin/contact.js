67d643114640147f37307b09b3f35c072f3fb32b
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-03-22 22:12:45 +0400

    contact form configuration

--- contactadmin/static/contactadmin/contact.js
+++ contactadmin/static/contactadmin/contact.js
@@ -28,32 +28,59 @@ const contact_firstname = document.getElementsByName('contact_firstname')
 const contact_lastname = document.getElementsByName('contact_lastname')
 const contact_email = document.getElementsByName('contact_email')
 const contact_message = document.getElementsByName('contact_message')
+const contactButtonSend = document.getElementById('contactButtonSend')
+
 const currentUrl = window.location.href
 
-console.log(contact_firstname)
-console.log(contact_lastname)
-console.log(contact_email)
-console.log(contact_message)
+const contactErrorShow = document.getElementById('contact-error-show')
 
+console.log(contact_message[0].value.length)//41 default
 //?inputlardaki plaholder deyeri yalniz id ile secilende isleyir name olanda kecersizidi yeni islemir placeholder
 // const fsname = document.getElementById('fsname')
 // console.log(fsname.placeholder)
 
+
 contactForm.addEventListener('submit',(e)=>{//butonu secmeye ehtiyac yoxdur burda cunki butonun type deyeri submit verende ele form tagi beraber olur butonun ozune yeni bir sozle submit edende butonu secmeye ehtiyac yoxdur,sadece addEventListener('submit') yazmag kifayet edir
     e.preventDefault()
-    $.ajax({
-        type:'POST',
-        url:currentUrl,
-        data:{
-            'csrfmiddlewaretoken':csrftoken,
-            'salamcontact':'Salam Men Contact Ajaxdan Gelmisem'
-        },
-        success:function(response){
-            console.log('Successfully Send POST CONTACT view')
-        },
-        error:function(err){
-            console.log('Xeta Var Amk')
-            console.log(err)
-        }
-    })
+
+    if(contact_message[0].value.length > 41){
+        $.ajax({
+            type:'POST',
+            url:currentUrl,
+            data:{
+                'csrfmiddlewaretoken':csrftoken,
+                'salamcontact':'Salam Men Contact Ajaxdan Gelmisem',
+                'contact_message':contact_message[0].value.trim()
+            },
+            success:function(response){
+                console.log('Successfully Send POST CONTACT view')
+                console.log(contact_message[0].value.length)
+                contactErrorShow.innerHTML = `
+                <div class="alert alert-success text-center text-capitalize" style="text-transform: capitalize;border-radius:3px;outline:none;">
+                    <strong style="color:black">Sucessfully Sent Request</strong>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            `
+                contactForm.reset()
+            },
+            error:function(err){
+                console.log('Error Have Send Contact')
+                console.log(err)
+            }
+        })
+    }
+    else{
+        contactErrorShow.innerHTML = `
+            <div class="alert alert-danger text-center text-capitalize" style="text-transform: capitalize;border-radius:3px;outline:none;">
+                <strong style="color:black">Please Fill Textarea</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        `
+        contactForm.reset()
+    }
+    
 })
--- contactadmin/views.py
+++ contactadmin/views.py
@@ -3,22 +3,19 @@ from .models import *
 from profiles.models import *
 
 # Create your views here.
+#*burada => @login_required decoratorundan istifade edersen insaAllah
 def contactView(request):
     profile = Profile.objects.get(user=request.user)
-    contactprof = ContactModel.objects.get(contact_prof=profile)
     
     if request.method == 'POST':
         print('POST ATILDI CONTACT')
-        print(request.POST.get('salamcontact'))
-    
-    print('Writing Profile First Name',contactprof.contact_prof.user.first_name)
-    print('Writing Profile Last Name',contactprof.contact_prof.user.last_name)
-    print('Writing Profile Email',contactprof.contact_prof.user.email)
-    print('Writing Profile Text',contactprof.contact_message)
-    
+        contact_message = request.POST.get('contact_message')
+        obj = ContactModel(contact_prof=profile,contact_message=contact_message)
+        obj.save()
+        
     
     context = {
-        'qs':'Hello Contact Page'
+        'profile':profile,
     }
     
     return render(request,'contactadmin/contact.html',context)
