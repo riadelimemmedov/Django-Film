72d31140449341ed37fc4ab2fd062142ef27f1e8
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-03-25 22:11:39 +0400

    api error configuration

--- film/settings.py
+++ film/settings.py
@@ -69,7 +69,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'profiles.context_processors.profilePicViewContext'#!
+                'profiles.context_processors.profilePicViewContext',
+                'movie.context_processors.getNewsView'
             ],
         },
     },
